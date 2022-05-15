ab9cff508e2925a33b1d3b3eede8d195056819b5
HARROUM Mouhamed Aymen <mouhamedhrr@gmail.com>
2022-05-16 01:36:18 +0200

    First Commit

---
+++ app/src/main/java/com/app/androidnewsapp/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.app.androidnewsapp.config;
+
+public class AppConfig {
+
+    //put your admin panel url here
+    public static final String ADMIN_PANEL_URL = "http://10.0.2.2/android_news_app";
+
+    //your api key which obtained from admin panel
+    public static final String API_KEY = "cda11v2OkqSI1rhQm37PBXKnpisMtlaDzoc4w0U6uNATgZRbJG";
+
+}
